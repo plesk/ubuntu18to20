08f78408e887847a60a26d90522003ffaee09d1f
Kirill Pushkarev <kirill.pushkarev@webpros.com>
2024-01-26 14:51:13 +0700

    Update copyright notices

--- ubuntu18to20/__init__.py
+++ ubuntu18to20/__init__.py
@@ -1,1 +1,1 @@
-# Copyright 1999-2023. Plesk International GmbH. All rights reserved.
+# Copyright 2023-2024. WebPros International GmbH. All rights reserved.
--- ubuntu18to20/config.py
+++ ubuntu18to20/config.py
@@ -1,4 +1,4 @@
-# Copyright 1999-2023. Plesk International GmbH. All rights reserved.
+# Copyright 2023-2024. WebPros International GmbH. All rights reserved.
 
 version = "@@UB18TO20_VERSION@@"
 revision = "@@UB18TO20_REVISION@@"
